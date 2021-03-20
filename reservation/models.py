8cf57f8daf9365122979f1181f5bf2bc283d37b3
Martin Timothy Timko <45509732+mttcode@users.noreply.github.com>
2021-03-20 12:58:41 +0100

    Create models.py

---
+++ reservation/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Reservation(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    phone = models.IntegerField()
+    number_of_persons = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+
+
+    def __str__(self):
+        return self.name
+
+
